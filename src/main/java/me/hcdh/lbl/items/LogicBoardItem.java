804c11ed26cd32fba723fad8d05a6c58f0766f43
DylanHedger <18100276+DylanHedger@users.noreply.github.com>
2020-05-30 06:00:59 +1000

    Changed LogicBoardItem.java (addInformation method)

--- src/main/java/me/hcdh/lbl/items/LogicBoardItem.java
+++ src/main/java/me/hcdh/lbl/items/LogicBoardItem.java
@@ -1,14 +1,19 @@
 package me.hcdh.lbl.items;
 
 import me.hcdh.lbl.LittleBigLogic;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class LogicBoardItem extends Item {
     public LogicBoardItem() {
@@ -35,8 +40,20 @@ public class LogicBoardItem extends Item {
             nbt.putBoolean("isCreated", true);
         }
 
-
         itemstack.setTag(nbt);
         return ActionResult.resultPass(playerIn.getHeldItem(handIn));
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        CompoundNBT nbt;
+        if (stack.hasTag())
+        {
+            nbt = stack.getTag();
+
+            if(nbt.getBoolean("isCreated")) {
+                tooltip.add(new StringTextComponent("\u00A7r\u00A7c\u00A7lLogic Board Created"));
+            }
+        }
     }
 }
